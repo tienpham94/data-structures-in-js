453255bd7d95b827d70d7a9d2e1d2f4f23b757ce
Tien Pham <r3dsky02@yahoo.com.vn>
2018-12-22 22:59:48 +0200

    q

---
+++ 1-queue.js
@@ -0,0 +1,39 @@
+function createQueue() {
+  const queue = []
+
+  return {
+    enqueue(x) {
+      queue.unshift(x)
+    },
+    dequeue() {
+      if (queue.length === 0) {
+        return undefined
+      }
+      return queue.pop()
+    },
+    peek() {
+      if (queue.length === 0) {
+        return undefined
+      }
+      return queue[queue.length - 1]
+    },
+    get length() {
+      return queue.length
+    },
+    isEmpty() {
+      return queue.length === 0
+    }
+  }
+}
+
+
+const q = createQueue()
+
+q.enqueue('Help others learn')
+q.enqueue('Be happy')
+
+q.dequeue()
+q.dequeue()
+console.log(q.peek())
+
+exports.createQueue = createQueue
